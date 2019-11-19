01f8b2ec241f88a677d03238b35a0ae43f29e469
smartworld <smartworld@email.com>
2019-11-19 21:39:30 +0100

    fix: store_bookings.py logic

--- store_bookings.py
+++ store_bookings.py
@@ -15,18 +15,19 @@ production = True  # Dev
 # production = False # Local
 
 if production:
-    DB_HOST = 'deliverme-db.cgc7xojhvzjl.ap-southeast-2.rds.amazonaws.com'
-    DB_USER = 'fmadmin'
-    DB_PASS = 'oU8pPQxh'
+    DB_HOST = "deliverme-db.cgc7xojhvzjl.ap-southeast-2.rds.amazonaws.com"
+    DB_USER = "fmadmin"
+    DB_PASS = "oU8pPQxh"
     DB_PORT = 3306
     # DB_NAME = 'dme_db_dev'  # Dev
-    DB_NAME = 'dme_db_prod'  # Prod
+    DB_NAME = "dme_db_prod"  # Prod
 else:
-    DB_HOST = 'localhost'
-    DB_USER = 'root'
-    DB_PASS = 'root'
+    DB_HOST = "localhost"
+    DB_USER = "root"
+    DB_PASS = "root"
     DB_PORT = 3306
-    DB_NAME = 'deliver_me'
+    DB_NAME = "deliver_me"
+
 
 def get_v_FPBookingNumber(v_FPBookingNumber):
     # if "_" in v_FPBookingNumber:
@@ -36,8 +37,9 @@ def get_v_FPBookingNumber(v_FPBookingNumber):
 
     return v_FPBookingNumber
 
+
 def get_booking_with_v_FPBookingNumber(v_FPBookingNumber, mysqlcon):
-     with mysqlcon.cursor() as cursor:
+    with mysqlcon.cursor() as cursor:
         sql = "SELECT * \
                 FROM `dme_bookings` \
                 WHERE `v_FPBookingNumber`=%s"
@@ -46,57 +48,63 @@ def get_booking_with_v_FPBookingNumber(v_FPBookingNumber, mysqlcon):
         # print('@102 - ', result)
         return result
 
+
 def do_process(fpath, mysqlcon):
     with open(fpath) as csv_file:
         for i, line in enumerate(csv_file):
-            v_FPBookingNumber = get_v_FPBookingNumber(line.split(',')[0])
-            fp_store_event_date = datetime.datetime.strptime(line.split(',')[2], '%Y-%m-%d')
-            fp_store_event_time = datetime.datetime.strptime(line.split(',')[3].replace('\n', ''), '%H:%M')
-
-            date = None
-            if not 'o' in line.split(',')[1]:
-                date = datetime.datetime.strptime(line.split(',')[1], '%Y-%m-%d')
-
+            v_FPBookingNumber = get_v_FPBookingNumber(line.split(",")[0])
+            fp_store_event_date = datetime.datetime.strptime(
+                line.split(",")[2], "%Y-%m-%d"
+            )
+            fp_store_event_time = datetime.datetime.strptime(
+                line.split(",")[3].replace("\n", ""), "%H:%M"
+            )
             booking = get_booking_with_v_FPBookingNumber(v_FPBookingNumber, mysqlcon)
 
             if booking:
-                print('@103 v_FPBookingNumber match: ', v_FPBookingNumber, date)
+                print("@103 v_FPBookingNumber match: ", v_FPBookingNumber, date)
                 with mysqlcon.cursor() as cursor:
-                    if date:
-                        sql = "UPDATE `dme_bookings` \
-                               SET de_Deliver_From_Date=%s, de_Deliver_By_Date=%s, fp_store_event_date=%s, fp_store_event_time=%s \
-                               WHERE id=%s"
-                        cursor.execute(sql, (date, date, fp_store_event_date, fp_store_event_time, booking['id']))
-                    else:
-                        sql = "UPDATE `dme_bookings` \
-                               SET fp_store_event_date=%s, fp_store_event_time=%s \
-                               WHERE id=%s"
-                        cursor.execute(sql, (fp_store_event_date, fp_store_event_time, booking['id']))
+                    sql = "UPDATE `dme_bookings` \
+                           SET de_Deliver_From_Date=%s, de_Deliver_By_Date=%s, fp_store_event_date=%s, fp_store_event_time=%s \
+                           WHERE id=%s"
+                    cursor.execute(
+                        sql,
+                        (
+                            fp_store_event_date,
+                            fp_store_event_date,
+                            fp_store_event_date,
+                            fp_store_event_time,
+                            booking["id"],
+                        ),
+                    )
                     mysqlcon.commit()
             else:
-                print('@104 v_FPBookingNumber not match: ', v_FPBookingNumber)
+                print("@104 v_FPBookingNumber not match: ", v_FPBookingNumber)
 
-if __name__ == '__main__':
-    print('#900 - Running %s' % datetime.datetime.now())
-    
+
+if __name__ == "__main__":
+    print("#900 - Running %s" % datetime.datetime.now())
+
     try:
-        mysqlcon = pymysql.connect(host=DB_HOST,
-                                   port=DB_PORT,
-                                   user=DB_USER,
-                                   password=DB_PASS,
-                                   db=DB_NAME,
-                                   charset='utf8mb4',
-                                   cursorclass=pymysql.cursors.DictCursor)
+        mysqlcon = pymysql.connect(
+            host=DB_HOST,
+            port=DB_PORT,
+            user=DB_USER,
+            password=DB_PASS,
+            db=DB_NAME,
+            charset="utf8mb4",
+            cursorclass=pymysql.cursors.DictCursor,
+        )
     except:
-        print('Mysql DB connection error!')
+        print("Mysql DB connection error!")
         exit(1)
 
     if production:
-        CSV_DIR = '/home/cope_au/dme_sftp/cope_au/store_bookings/indata/'
-        ARCHIVE_DIR = '/home/cope_au/dme_sftp/cope_au/store_bookings/archive/'
+        CSV_DIR = "/home/cope_au/dme_sftp/cope_au/store_bookings/indata/"
+        ARCHIVE_DIR = "/home/cope_au/dme_sftp/cope_au/store_bookings/archive/"
     else:
-        CSV_DIR = '/Users/admin/work/goldmine/scripts/dir01/'
-        ARCHIVE_DIR = '/Users/admin/work/goldmine/scripts/dir02/'
+        CSV_DIR = "/Users/admin/work/goldmine/scripts/dir01/"
+        ARCHIVE_DIR = "/Users/admin/work/goldmine/scripts/dir02/"
 
     if not os.path.isdir(CSV_DIR):
         print('Given argument "%s" is not a directory' % CSV_DIR)
@@ -106,14 +114,14 @@ if __name__ == '__main__':
         for fname in os.listdir(CSV_DIR):
             fpath = os.path.join(CSV_DIR, fname)
 
-            if os.path.isfile(fpath) and fname.endswith('.csv'):
-                print('@100 Detect csv file:', fpath)
+            if os.path.isfile(fpath) and fname.endswith(".csv"):
+                print("@100 Detect csv file:", fpath)
                 do_process(fpath, mysqlcon)
                 shutil.move(CSV_DIR + fname, ARCHIVE_DIR + fname)
-                print('@109 Moved csv file:', fpath)
+                print("@109 Moved csv file:", fpath)
 
     except OSError as e:
         print(str(e))
 
-    print('#901 - Finished %s' % datetime.datetime.now())
+    print("#901 - Finished %s" % datetime.datetime.now())
     mysqlcon.close()
