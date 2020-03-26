16915fefe632ea4eb5fa18d5959932a1e5498195
smartworld-dm <smartworld@email.com>
2020-03-26 15:10:12 +0100

    fix: hunter_status_pod

--- hunter_status_pod.py
+++ hunter_status_pod.py
@@ -14,8 +14,8 @@ if IS_PRODUCTION:
     DB_USER = "fmadmin"
     DB_PASS = "oU8pPQxh"
     DB_PORT = 3306
-    DB_NAME = "dme_db_dev"  # Dev
-    # DB_NAME = "dme_db_prod"  # Prod
+    # DB_NAME = "dme_db_dev"  # Dev
+    DB_NAME = "dme_db_prod"  # Prod
 else:
     DB_HOST = "localhost"
     DB_USER = "root"
@@ -24,8 +24,8 @@ else:
     DB_NAME = "deliver_me"
 
 if IS_PRODUCTION:
-    API_URL = "http://3.105.62.128/api"  # Dev
-    # API_URL = "http://13.55.64.102/api"  # Prod
+    # API_URL = "http://3.105.62.128/api"  # Dev
+    API_URL = "http://13.55.64.102/api"  # Prod
 else:
     API_URL = "http://localhost:8000/api"  # Local
 
@@ -34,12 +34,12 @@ def get_bookings(mysqlcon):
     with mysqlcon.cursor() as cursor:
         sql = "SELECT `id`, `b_bookingID_Visual`, `b_error_Capture` \
                 FROM `dme_bookings` \
-                WHERE `vx_freight_provider`=%s and `z_api_issue_update_flag_500`=%s and \
-                (`b_status` is NULL or (`b_status`<>%s and `b_status`<>%s)) and \
+                WHERE `vx_freight_provider`=%s and \
+                (`b_status` is NULL or (`b_status`<>%s and `b_status`<>%s and `z_pod_url` is NULL)) and \
                 (`b_error_Capture` is NULL or `b_error_Capture`=%s) \
                 ORDER BY id DESC \
-                LIMIT 10"
-        cursor.execute(sql, ("Hunter", "1", "Ready for booking", "Delivered", ""))
+                LIMIT 25"
+        cursor.execute(sql, ("Hunter", "Ready for booking", "Closed", ""))
         bookings = cursor.fetchall()
 
         return bookings
@@ -70,12 +70,13 @@ def do_pod(booking):
 
 
 def do_process(mysqlcon):
-    # Get 3 Hunter bookings
+    # Get 25 Hunter bookings
     bookings = get_bookings(mysqlcon)
     print("#200 - Booking cnt to process: ", len(bookings))
 
     if len(bookings) > 0:
         for booking in bookings:
+            time.sleep(10)
             print("#200 - Processing: ***", booking["b_bookingID_Visual"], "***")
             result = do_tracking(booking)
 
