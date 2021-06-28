b459ab4dfe508fb9d83b0b92f1626df632c34117
smartworld-dm <smartworld@email.com>
2021-06-29 01:27:09 +0200

    fix: <>%s AND

--- allied_status_pod.py
+++ allied_status_pod.py
@@ -16,7 +16,7 @@ def get_in_progress_bookings(mysqlcon):
                 WHERE `vx_freight_provider`=%s \
                     AND (`b_client_name`=%s OR `b_client_name`=%s) \
                     AND (`z_lock_status`=%s OR `z_lock_status` IS NULL) \
-                    AND (`b_status`<>%s AND `b_status`<>%s AND `b_status`<>%s AND `b_status`<>%s) \
+                    AND (`b_status`<>%s AND `b_status`<>%s AND `b_status`<>%s AND `b_status`<>%s AND `b_status`<>%s) \
                 ORDER BY id DESC \
                 LIMIT 2"
         cursor.execute(
