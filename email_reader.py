c4debd1f9f9400b21ca90976f555f02767d28c27
smartworld-dm <smartworld@email.com>
2021-08-20 03:34:54 +0200

    add: pete's email to No Quote email

--- email_reader.py
+++ email_reader.py
@@ -206,7 +206,11 @@ When complete click 'Update' and then 'Price & Time Calc (FC)'. Select the appro
 Your booking will then be ready for the warehouse to process."
             send_email(
                 ["customerservice@jasonl.com.au"],
-                ["stephenm@deliver-me.com.au", "dev.deliverme@gmail.com"],
+                [
+                    "stephenm@deliver-me.com.au",
+                    "petew@deliver-me.com.au",
+                    "dev.deliverme@gmail.com",
+                ],
                 f"No Quotes",
                 text,
             )
