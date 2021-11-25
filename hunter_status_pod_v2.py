dd3401de6ee2037f4c69115ed57cba9da8a61aff
daniel <astar1112.rich@outlook.com>
2021-11-25 10:36:58 +0800

    update hunter tracking

--- hunter_status_pod_v2.py
+++ hunter_status_pod_v2.py
@@ -21,7 +21,9 @@ from _env import (
     HT_ARCHIVE_DIR as ARCHIVE_DIR,
     HT_ISSUED_DIR as ISSUED_DIR,
     API_URL,
-    S3_URL
+    S3_URL,
+    USERNAME,
+    PASSWORD
 )
 from _options_lib import get_option, set_option
 from _upload_download import download_sftp, upload_sftp
@@ -38,19 +40,32 @@ sftp_server_infos = {
     "local_filepath_archive": ARCHIVE_DIR,
 }
 
+def get_token():
+    url = API_URL + "/api-token-auth/"
+    data = {"username": USERNAME, "password": PASSWORD}
+    response = requests.post(url, params={}, json=data)
+    response0 = response.content.decode("utf8")
+    data0 = json.loads(response0)
+
+    if "token" in data0:
+        print("@101 - Token: ", data0["token"])
+        return data0["token"]
+    else:
+        print("@400 - ", data0["non_field_errors"])
+        return None
+
+def get_booking(consignment_number, mysqlcon):
+    with mysqlcon.cursor() as cursor:
+        sql = "SELECT `id`, `pk_booking_id`, `vx_freight_provider` From `dme_bookings` WHERE `v_FPBookingNumber`=%s"
+        cursor.execute(sql, (consignment_number))
+        booking = cursor.fetchone()
+    return booking
 
 def update_booking(consignment_number, pod_url, mysqlcon):
     with mysqlcon.cursor() as cursor:
         sql = "UPDATE dme_bookings SET z_pod_url=%s WHERE `v_FPBookingNumber`=%s"
         result = cursor.execute(sql, (pod_url, consignment_number))
     return result
-
-def get_booking(consignment_number, mysqlcon):
-    with mysqlcon.cursor() as cursor:
-        sql = "SELECT `id`, `vx_freight_provider` From `dme_bookings` WHERE `v_FPBookingNumber`=%s"
-        cursor.execute(sql, (consignment_number))
-        booking = cursor.fetchone()
-    return booking
 
 def do_processing(mysqlcon):
     # Download .FTP files
@@ -67,59 +82,67 @@ def do_processing(mysqlcon):
         print("Failed download .FTP files from remote. Error: ", str(e))
         set_option(mysqlcon, "hunter_status_pod", False, time1)
 
-
+    token = get_token()
     for file in listdir(INPROGRESS_DIR):
         have_issue = False
         if 'ConsignmentStatusUpdate' in file:
             con_index = -1
             with open(path.join(INPROGRESS_DIR, file), 'r') as csvfile:
                 csv_content = list(csv.reader(csvfile))
+                cols = csv_content[0]
+                content = csv_content[1:]  
+                print("\nFile: ", file, "\nCols: ", cols, "\nContent: ", content)
+
                 try:
-                    header = csv_content[0]
-                    con_index = header.index('ConsignmentNumber')
-                    time_index = header.index('StatusDateTime')
-                    status_index = header.index('Status')
+                    con_index = cols.index('ConsignmentNumber')
+                    time_index = cols.index('StatusDateTime')
+                    status_index = cols.index('Status')
                 except Exception as e:
                     have_issue = True
                     print('Invalid file type: ', file)
-            if con_index == 0:
-                content = csv_content[1:]
-                for index, line in enumerate(content):
-                    consignment_number = line[con_index]
-                    fp_status_code = line[status_index]
-                    event_time_stamp = datetime.strptime(line[time_index], '%d/%m/%Y %H:%M %z').strftime('%Y-%m-%d %H:%M:%S.%f')
 
-                    if consignment_number:
-                        booking = get_booking(consignment_number, mysqlcon)
-                    else:
-                        print('No consignment number: ', file, ', Row: ', index + 1)
-                        have_issue = True
+                if con_index == 0:
+                    for index, line in enumerate(content):
+                        consignment_number = line[con_index]
+                        fp_status_code = line[status_index]
+                        event_time_stamp = datetime.strptime(line[time_index], '%d/%m/%Y %H:%M %z').strftime('%Y-%m-%d %H:%M:%S.%f')
 
-                    if (booking 
-                        and booking['vx_freight_provider']
-                        and booking['vx_freight_provider'].lower() == 'hunter'
-                    ):
-                        headers = {
-                            "content-type": "application/json",
-                        }
-                        requests.post(f"{API_URL}/statushistory/save_status_history/", headers=headers, data=json.dumps({
-                            'booking_id': booking['id'],
-                            'consignment_number': consignment_number,
-                            'fp_status_code': fp_status_code,
-                            'fp_status_details': None,
-                            'event_time_stamp': event_time_stamp,
-                            'is_from_script': True
-                        }))
-                    else:
-                        print('No booking or wrong freight_provider: ', file, ', Row: ', index + 1)
-                        have_issue = True
+                        if consignment_number:
+                            booking = get_booking(consignment_number, mysqlcon)
+                        else:
+                            print('No consignment number: ', file, ', Row: ', index + 1)
+                            have_issue = True
+
+                        if (booking 
+                            and booking['vx_freight_provider']
+                            and booking['vx_freight_provider'].lower() == 'hunter'
+                        ):
+                            headers = {
+                                "content-type": "application/json",
+                                "Authorization": f"JWT {token}",
+                            }
+                            response = requests.post(f"{API_URL}/statushistory/save_status_history/", headers=headers, data=json.dumps({
+                                "fk_booking_id": booking["pk_booking_id"],
+                                'consignment_number': consignment_number,
+                                'fp_status': fp_status_code,
+                                'fp_status_description': None,
+                                'event_time_stamp': event_time_stamp,
+                                'is_from_script': True
+                            }))
+                            has_issue = response.status_code != 200
+                        else:
+                            print('No booking or wrong freight_provider: ', file, ', Row: ', index + 1)
+                            have_issue = True
         elif 'Pod' in file:
             with open(path.join(INPROGRESS_DIR, file), 'r') as csvfile:
                 csv_content = list(csv.reader(csvfile))
-                row_count = len(csv_content)
-                for index in range(1, row_count):
-                    consignment_number = csv_content[index][0]
-                    image = csv_content[index][8]
+                cols = csv_content[0]
+                content = csv_content[1:]  
+                print("\nFile: ", file, "\nCols: ", cols, "\nContent: ", content)
+
+                for index, line in enumerate(content):
+                    consignment_number = line[0]
+                    image = line[8]
                 
                     if consignment_number:
                         booking = get_booking(consignment_number, mysqlcon)
@@ -136,7 +159,8 @@ def do_processing(mysqlcon):
                         with open(full_path, "wb") as f:
                             f.write(base64.b64decode(image))
                             f.close()
-                            update_booking(consignment_number, db_pod_url, mysqlcon)
+                        result = update_booking(consignment_number, db_pod_url, mysqlcon)
+                        have_issue = not result
 
                     else:
                         print('No booking or wrong freight_provider: ', file, 'row: ', index)
@@ -182,9 +206,7 @@ if __name__ == "__main__":
             print("#906 - `hunter_status_pod` option is ON")
             set_option(mysqlcon, "hunter_status_pod", True)
             print("#910 - Processing...")
-
             do_processing(mysqlcon)
-
             set_option(mysqlcon, "hunter_status_pod", False, time1)
     except Exception as e:
         print("Error 904:", str(e))
