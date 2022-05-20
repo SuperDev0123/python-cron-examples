1e529cdd0bcb73c2c9d3f3b17d64545be4c075d3
smartworld-dm <smartworld@email.com>
2022-05-20 03:58:02 +0200

    mod: get Token when has new emails

--- email_reader.py
+++ email_reader.py
@@ -443,7 +443,9 @@ def do_process(mysqlcon):
 
     print("@800 - Reading 50 recent emails...")
     emails = read_email_from_gmail()
-    token = get_token()
+
+    if emails:
+        token = get_token()
 
     for email in emails:
         subject = email["subject"]
